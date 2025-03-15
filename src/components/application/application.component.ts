f33152b87a7693c7136e4a3e64a5690b2bfc1e4b
kushanz <kushanshanuka@yahoo.com>
2025-03-15 16:44:20 +0530

    INIT APP COMPONENT

--- src/components/application/application.component.ts
+++ src/components/application/application.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'application',
-  imports: [ButtonModule],
+  imports: [CommonModule, InputTextModule, FormsModule, MessageModule, ButtonModule],
   templateUrl: './application.component.html',
   styleUrl: './application.component.css'
 })
 export class ApplicationComponent {
+  text = '';
 
+  msg = '';
+
+  onClick() {
+    this.msg = 'Welcome ' + this.text;
+  }
 }
